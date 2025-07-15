e21066fa156d44a34cc9522c2a190b7a41ab2c4a
Lumber4ack <protsyukvitalik@gmail.com>
2025-07-16 01:10:05 +0200

    added styles for buttons

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -10,63 +10,74 @@ const MetaPrefix = "Classification";
 let ListSuffix = ["Document", "Default", "Restricted","Protected"];
 ListSuffix.push("NoLabel");
 
-function recolorButtons(activeButtonId, color = "#00FF00", defaultColor = "#000000") {
-  const buttons = document.querySelectorAll(".meta-a_button");
+function recolorButtons(activeButtonId, color = "meta-button-active") {
+  const buttons = document.querySelectorAll(".pref-button");
   buttons.forEach(btn => {
-    btn.style.color = (btn.id === activeButtonId) ? color : defaultColor;
+    if (btn.id === activeButtonId) {
+      btn.classList.add(color);
+    } else {
+      btn.classList.remove(color);
+    }
   });
 }
+
+function createButton(suffix = "") {
+  const button = document.createElement("button");
+  button.id = suffix;
+  button.className = "pref-button meta-button";
+  button.textContent = suffix;
+  button.onclick = () => {
+        recolorButtons(button.id);
+        addCustomProperty(MetaPrefix, suffix);
+      };
+  return button;
+}
+
 Office.onReady(async (info) => {
   window.INFO = info;
   initCustomProp();
   if (info.host === Office.HostType.Word || info.host === Office.HostType.Excel || (info.host === Office.HostType.PowerPoint && Office.context.requirements.isSetSupported("PowerPointApi", "1.7"))) {
+    
     const res = await fetch('https://192.168.128.4:8000/list');
     const resJson = await res.json();
     ListSuffix = await resJson.names;
+    
     console.log(JSON.stringify(ListSuffix,null,2));
+    
     if(!ListSuffix.includes("NoLabel")){
       ListSuffix.push("NoLabel");
     }
 
     document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = runDocument;
+    //document.getElementById("run").onclick = runDocument;
 
     for (const suffix of ListSuffix) {
-      const newButton = document.createElement("button");
-      newButton.id = suffix;
-      newButton.className = "meta-a_button ms-font-xl";
-      newButton.textContent = suffix;
-      newButton.onclick = () => {
-        recolorButtons(newButton.id);
-        addCustomProperty(MetaPrefix, suffix);
-      };
+      const newButton = createButton(suffix);
       document.getElementById("app-body").appendChild(newButton);
     }
+
     document.getElementById("NoLabel").onclick = () => {
       recolorButtons("NoLabel");
       addCustomProperty(MetaPrefix, "", "NoLabel");
     };
-    var rb = await readCustomProperty(MetaPrefix);
-    console.log(`Read custom property "${MetaPrefix}": ${rb}`);
-    if (ListSuffix.includes(rb)) {
-      console.log(`Custom property "${MetaPrefix}" exists with value: ${rb}`);
-      document.getElementById(rb).style.color = "#00FF00";
-    } else if (rb === null || rb === "") {
+
+    var prefixValue = await readCustomProperty(MetaPrefix);
+
+    console.log(`Read custom property "${MetaPrefix}": ${prefixValue}`);
+    if (ListSuffix.includes(prefixValue)) { // If the custom property value is in the list
+      console.log(`Custom property "${MetaPrefix}" exists with value: ${prefixValue}`);
+      document.getElementById(prefixValue).classList.add("meta-button-active");
+    } 
+    else if (prefixValue === null || prefixValue === "") { // If the custom property does not exist or is empty
       console.log(`Custom property "${MetaPrefix}" exists with value: "NoLabel"`);
-      document.getElementById("NoLabel").style.color = "#00FF00";
-    } else {
-      const newButton = document.createElement("button");
-      newButton.id = rb;
-      newButton.className = "meta-a_button ms-font-xl";
-      newButton.textContent = rb;
-      newButton.style.color = "#00FF00";
-      newButton.onclick = () => {
-        recolorButtons(newButton.id);
-        addCustomProperty(MetaPrefix, rb, newButton.id);
-      };
-      const referenceElement = document.getElementById("NoLabel");
-      document.getElementById("app-body").insertBefore(newButton, referenceElement);
+      document.getElementById("NoLabel").classList.add("meta-button-active");
+    } 
+    else { // If the custom property exists but is not in the list
+      console.log(`Custom property "${MetaPrefix}" exists with value: ${prefixValue}`);
+      newButton = createButton(prefixValue)
+      document.getElementById("app-body").insertBefore(newButton, document.getElementById("NoLabel"));
     };
+    /*
     document.getElementById("add-prop").onclick = () => {
       const value = document.getElementById("custom-prop-value").value;
       addCustomProperty(MetaPrefix, value, );
@@ -75,11 +86,12 @@ Office.onReady(async (info) => {
       const value = document.getElementById("remove-prop-value").value;
       removeCustomProperty(value);
     };
+    */
   }
 });
 
 
-
+/*
 export async function runDocument() {
   return Word.run(async (context) => {
     const properties = context.document.properties.customProperties;
@@ -109,3 +121,4 @@ export async function runPresentation() {
     console.log(JSON.stringify(properties.items, null, 2));
 });
 }
+*/
