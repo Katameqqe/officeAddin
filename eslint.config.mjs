478f3486a4cf6ec54d090cf6dda65269f5fcc0d2
Danil Korotenko <danil.korotenko@gmail.com>
2025-08-22 17:21:44 +0300

    eslint config has been added.

---
+++ eslint.config.mjs
@@ -0,0 +1,72 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default
+[
+    {
+        files: ["**/*.js",],
+        languageOptions: {sourceType: "commonjs",},
+    },
+    {
+        languageOptions:
+        {
+            globals:
+            {
+                console: "readonly",
+                document: "readonly",
+                expect: "readonly",
+                it: "readonly",
+                jest: "readonly",
+                Office: "readonly",
+                test: "readonly",
+            },
+        },
+    },
+    pluginJs.configs.recommended,
+    {
+        rules:
+        {
+            'key-spacing': ["error", { "beforeColon": false, },],
+            "object-shorthand": ["off",],
+            'brace-style': ['error', 'allman',],
+            'eol-last': ["error", "always",],
+            indent:
+            [
+                'error', 4,
+                {
+                    outerIIFEBody: 1,
+                    FunctionExpression: { body: 1, parameters: 2, },
+                    SwitchCase: 1,
+                },
+            ],
+            'no-unused-vars': ['off', { vars: 'local', },],
+            'no-multi-spaces': ['off',],
+            'no-trailing-spaces': "error",
+            quotes: ["off",],
+            "space-before-function-paren": ["off",],
+            "comma-dangle":
+            [
+                "error",
+                {
+                    arrays: "always",
+                    objects: "always",
+                    imports: "always",
+                    exports: "always",
+                    functions: "never",
+                },
+            ],
+            "keyword-spacing":
+            [
+                "error",
+                {
+                    overrides:
+                    {
+                        catch: { after: false, },
+                    },
+                },
+            ],
+        },
+    },
+];
